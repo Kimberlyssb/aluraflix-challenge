a1435de390a27ca36f55cf7e94fb70087a054975
Kimberlyssb <kimberlysarai@gmail.com>
2024-08-20 14:24:27 -0300

    actualiza ruta img footer

--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -3,7 +3,7 @@ import "./Footer.css"
 function Footer (){
     return(
         <footer>
-            <img className="img-footer" src="/public/img/Logo.png" alt="logo"/>
+            <img className="img-footer" src="/img/Logo.png" alt="logo"/>
             <h1>Creado por Kimberly Soto 2024</h1>
         </footer>
     )
